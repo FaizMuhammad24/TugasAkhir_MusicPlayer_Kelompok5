eb49329304e8b6116a8cdd50a65c724c7c0a9e84
Faiz Muhammad <tubagusrofi0810@gmail.com>
2023-04-04 12:10:46 +0700

    Add Detail Lagu and List Lagu

--- app/src/main/java/com/example/music_player_api/MainActivity.kt
+++ app/src/main/java/com/example/music_player_api/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.music_player_api
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        pindah()
+        pindah1()
     }
+    fun pindah() {
+        val cardNext: CardView = findViewById(R.id.card2)
+        cardNext.setOnClickListener {
+            val intent = Intent(this@MainActivity, ListLaguActivity::class.java)
+            startActivity(intent)
+        }
+    }
+    fun pindah1() {
+        val textNext: TextView = findViewById(R.id.TextRadio)
+        textNext.setOnClickListener {
+            val intent = Intent(this@MainActivity, RadioActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
 }
